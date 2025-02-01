f027219c5687dee564e4c43ff2c4929fa4da868a
pls11dsp <pls11@dspcontroles.com.br>
2025-02-01 15:36:01 -0300

    Project created

---
+++ src/main/java/com/dspcontroles/course5/Course5Application.java
@@ -0,0 +1,13 @@
+package com.dspcontroles.course5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Course5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/dspcontroles/course5/Course5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dspcontroles.course5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Course5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
