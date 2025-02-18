e767f41eab0e751f05dd5fb19d6ce4332c988a66
pls11dsp <pls11@dspcontroles.com.br>
2025-02-18 11:52:11 -0300

    Exception handling - update

--- src/main/java/com/dspcontroles/course5/services/UserService.java
+++ src/main/java/com/dspcontroles/course5/services/UserService.java
@@ -13,6 +13,8 @@ import com.dspcontroles.course5.repositories.UserRepository;
 import com.dspcontroles.course5.services.exceptions.DatabaseException;
 import com.dspcontroles.course5.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -43,9 +45,13 @@ public class UserService {
 	}
 		
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 		
 	}
 
