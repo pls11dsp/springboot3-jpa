5a7702f62b3bb91372cc2ec65ec779d32182d9c3
pls11dsp <pls11@dspcontroles.com.br>
2025-02-18 08:12:14 -0300

    Exception handling - delete

--- src/main/java/com/dspcontroles/course5/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/dspcontroles/course5/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.dspcontroles.course5.services.exceptions.DatabaseException;
 import com.dspcontroles.course5.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -17,8 +18,18 @@ public class ResourceExceptionHandler {
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, 
 			HttpServletRequest request){
-			String error = "Resouce Not Found";
+			String error = "Resource Not Found";
 			HttpStatus status = HttpStatus.NOT_FOUND;
+			StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),
+			request.getRequestURI());
+			return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, 
+			HttpServletRequest request){
+			String error = "Database error";
+			HttpStatus status = HttpStatus.BAD_REQUEST;
 			StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),
 			request.getRequestURI());
 			return ResponseEntity.status(status).body(err);
